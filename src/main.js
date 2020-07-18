b57f22e05f38276589a53840cd778c08304c54a0
sanix sandel <sanicksikani@gmail.com>
2020-07-18 15:01:09 +0300

    RestFul service

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import BootstrapVue from 'bootstrap-vue';
+import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "font-awesome/css/font-awesome.min.css";
+
+Vue.use(BootstrapVue)
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
