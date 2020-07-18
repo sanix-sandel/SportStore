a446ba72d60db3a9696645f1c86329d6b090f0cd
sanix sandel <sanicksikani@gmail.com>
2020-07-18 17:36:01 +0300

    first part completed

--- src/components/Store.vue
+++ src/components/Store.vue
@@ -7,7 +7,7 @@
         </div>
         <div class="row">
             <div class="col-3 bg-info p-2">
-                <h4 class="text-white m-2">Categories</h4>
+                <CategoryControls/>
             </div>
             <div class="col-9 p-2">
                 <Product-list/><!-- Or just use ProductList -->
@@ -18,9 +18,10 @@
 
 <script>
     import ProductList from "./ProductList";
+    import CategoryControls from "./CategoryControls";
 
     export default{
-        components:{ProductList}
+        components:{ProductList, CategoryControls}
     }
 
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -18,16 +18,26 @@ export default new Vuex.Store({
         products:testData,
         productsTotal:testData.length,
         currentPage:1,
-        pageSize:4
+        pageSize:4,
+        currentCategory:"All"
     },
 
     getters:{
-        processedProducts:state=>{
+        productsFilteredByCategory:state=>state.products
+            .filter(p=>state.currentCategory=="All"
+                ||p.category==state.currentCategory),
+
+        processedProducts:(state, getters)=>{
             let index=(state.currentPage -1)*state.pageSize;
-            return state.products.slice(index, index+state.pageSize);
+            return getters.productsFilteredByCategory
+                .slice(index, index+state.pageSize);
         },
 
-        pageCount:state=>Math.ceil(state.productsTotal/state.pageSize)
+        pageCount:(state, getters)=>
+            Math.ceil(getters.productsFilteredByCategory.length/state.pageSize),
+
+        categories:state=>["All",
+            ...new Set(state.products.map(p=>p.category).sort())]
     },
     mutations:{
         setCurrentPage(state, page){
@@ -35,6 +45,11 @@ export default new Vuex.Store({
         },
         setPageSize(state, size){
             state.pageSize=size;
+            state.currentPage=1;
+        },
+
+        setCurrentCategory(state, category){
+            state.currentCategory=category;
             state.currentPage=1;
         }
     }
