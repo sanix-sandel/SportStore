0d80359acbdef1f6a224d542cb61cce6d7f13f1a
sanix sandel <sanicksikani@gmail.com>
2020-07-18 16:29:26 +0300

    changing the page size

--- src/components/PageControls.vue
+++ src/components/PageControls.vue
@@ -1,13 +1,23 @@
 <template>
-    <div v-if="pageCount > 1" class="text-right">
-        <div class="btn-group mx-2">
-            <button v-for="i in pageNumbers" v-bind:key="i"
-                    class="btn btn-secpmdary"
-                    v-bind:class="{ 'btn-primary': i == currentPage }"
-                    v-on:click="setCurrentPage(i)">
-                {{ i }}
-            </button>
+
+    <div class="row mt-2">
+        <div class="col form-group">
+            <select class="form-control" v-on:change="changePageSize">
+                <option value="4">4 per Page</option>
+                <option value="8">8 per page</option>
+                <option value="12">12 per page</option>
+            </select>
         </div>
+        <div class="text-right col">
+            <div class="btn-group mx-2">
+                <button v-for="i in pageNumbers" v-bind:key="i"
+                        class="btn btn-secpmdary"
+                        v-bind:class="{ 'btn-primary': i == currentPage }"
+                        v-on:click="setCurrentPage(i)">
+                    {{ i }}
+                </button>
+            </div>
+        </div>    
     </div>
 </template>
 <script>
@@ -24,7 +34,10 @@
             }
         },
         methods:{
-            ...mapMutations(["setCurrentPage"])
+            ...mapMutations(["setCurrentPage", "setPageSize"]),
+            changePageSize($event){
+                this.setPageSize(Number($event.target.value));
+            }
         }
     }
 </script>
