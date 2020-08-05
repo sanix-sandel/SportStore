b0fb23c9d51cf84839e3450a8a6f293c7b3b8782
sanix sandel <sanicksikani@gmail.com>
2020-08-05 13:16:50 +0300

    VueRouter

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+import Store from "../components/Store";
+import ShoppingCart from "../compoennets/ShoppingCart";
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+    mode:"history",
+    routes:[
+        {path:"/", componen:Store},
+        {path:"/cart", component:ShoppingCart},
+        {path:"*", redirect:"/"}
+    ]
+})
